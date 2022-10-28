4b961fa2b4f578a3d2288ee0acdf05d1bb2ac50b
zaabsyid <abdulrosyid2002@gmail.com>
2022-10-28 15:03:52 +0700

    Initial commit first

---
+++ app/src/main/java/com/zahirar/ch7tpc2authtest/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zahirar.ch7tpc2authtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
